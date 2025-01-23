d590f93cf22f08143401fec33c3ba8d1b6b3b97e
JshWasTaken2 <owbigshaq@hotmail.com>
2025-01-24 00:49:55 +0900

    Update server.js

--- server.js
+++ server.js
@@ -88,6 +88,24 @@ app.get("/randomline", async (req, res) => {
     res.send(responseMessage);
 });*/
 
+// Add a route for /api/fight
+app.get('/api/fight', (req, res) => {
+  const { sender, touser, randomChatter } = req.query;
+
+  // Validate query parameters
+  if (!sender) {
+    return res.status(400).send("Missing 'sender' parameter");
+  }
+
+  // Determine the target
+  const target = touser || randomChatter || "someone";
+
+  // Response message
+  const message = `${sender} picked a fight with ${target} $(urlfetch https://pastebin.com/raw/nwYG6VsA!)`;
+
+  res.status(200).send(message);
+});
+
 
 
 
